0df607351bf4f97a0e7659e10a5f1537d8c3e657
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-01-24 13:08:07 +0100

    change template for react

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+    IonButton,
+    IonContent,
+    IonHeader,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonPage,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import {useState} from "react";
+import axios from "axios";
+
 
 const Tab1: React.FC = () => {
+    const [joke,setJokes]= useState()
+
+    function changeJokes(){
+        axios.get("https://api.chucknorris.io/jokes/random")
+            .then((response)=>{
+                setJokes(response.data.value)
+                console.log(response.data.value)
+            })
+    }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>jokes</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
+        <IonContent fullscreen>
+            <IonHeader collapse="condense">
+                <IonToolbar>
+                    <IonTitle size="large">Tab 1</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonButton onClick={changeJokes} className="btn btn-success">Change jokes</IonButton>
+            <br/>
+            <IonList>
+                <IonItem>
+                    <IonLabel>{joke}</IonLabel>
+                </IonItem>
+            </IonList>
+        </IonContent>
     </IonPage>
   );
 };
